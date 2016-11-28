8c984629a3ba98f42a44857d602687c7b96ebe58
Logan Franken <loganfranken@gmail.com>
2016-11-28 09:48:31 -0800

    Removed vendor prefixes, added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,30 @@
 var childProcess = require('child_process');
+var cssmin = require('gulp-cssmin');
 var del = require('del');
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
+var postcss = require('gulp-postcss');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', ['minify-js', 'jekyll']);
+gulp.task('default', ['process-css', 'minify-js', 'jekyll']);
+
+gulp.task('process-css', function() {
+
+  return gulp.src('./style/main.scss')
+    .pipe(sass())
+    .pipe(postcss([ require('autoprefixer') ]))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./style/'));
+
+});
 
 gulp.task('minify-js', function () {
 
   gulp.src(['./scripts/*.js', '!./scripts/*.min.js'])
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('./scripts/'));
 
 });
